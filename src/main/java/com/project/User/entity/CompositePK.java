e563a2ed1a021fe21c693b09665602e76e6a9ff7
Ankita678 <59177092+Ankita678@users.noreply.github.com>
2021-03-15 18:22:56 +0530

    Update CompositePK.java

--- src/main/java/com/project/User/entity/CompositePK.java
+++ src/main/java/com/project/User/entity/CompositePK.java
@@ -8,8 +8,6 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 @EqualsAndHashCode
 public class CompositePK implements Serializable{
 
